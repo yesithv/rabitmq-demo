dd2fe66860689a1e92c0507a25493b715b4f7c91
yvalencia <yesithvalencia@gmail.com>
2025-08-04 17:44:18 -0500

    add others files

---
+++ src/main/java/yesithv/rabitmqdemo/RabitmqDemoApplication.java
@@ -0,0 +1,13 @@
+package yesithv.rabitmqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabitmqDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabitmqDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yesithv/rabitmqdemo/RabitmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package yesithv.rabitmqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabitmqDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
